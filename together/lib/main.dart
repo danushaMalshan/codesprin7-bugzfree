a2f7a0d1497003f4efc7f1382d18f5fc2da51389
vinukavinnath <wlvvinnath@gmail.com>
2023-03-30 20:00:35 +0530

    Signup redesigned

--- together/lib/main.dart
+++ together/lib/main.dart
@@ -1,100 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:together/screens/auth/signup.dart';
 
 void main() {
   runApp(
-    SignUp(),
+   const MyApp()
   );
 }
 
-class SignUp extends StatefulWidget {
-  const SignUp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<SignUp> createState() => _SignUpState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-Widget loginButtons(
-    String title, String imagePath, Color titleColor, Color backColor) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 30),
-    child: Container(
-      child: ListTile(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        tileColor: backColor,
-        leading: Image.asset(imagePath,height: 30,),
-        title: Center(
-          child: Text(
-            title,
-            style: TextStyle(
-              color: titleColor,
-              fontSize: 18,
-            ),
-          ),
-        ),
-        trailing:Icon(Icons.ice_skating,color:Colors.transparent),
-      ),
-    ),
-  );
-}
-
-class _SignUpState extends State<SignUp> {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Image(
-                image: AssetImage("Assets/Images/Logo.jpg"),
-                height: 350,
-                width: 300,
-              ),
-              SizedBox(height: 25),
-              loginButtons("Continue with Apple",
-                  "Assets/Images/apple_logo.png", Colors.white, Colors.black),
-              loginButtons(
-                "Continue with Facebook",
-                "Assets/Images/facebook.png",
-                Colors.white,
-                Color(0xff1877f2),
-              ),
-              loginButtons(
-                "Continue with Google",
-                "Assets/Images/google.png",
-                Color(0xff142867),
-                Color(0xffd9d9d9),
-              ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 30),
-          child: Container(
-            child: ListTile(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              tileColor: Color(0xffd9d9d9),
-              leading: Icon(Icons.mail_outline_rounded,size:30),
-              title: Center(
-                child: Text(
-                  "Continue with Email",
-                  style: TextStyle(
-                    color: Color(0xff142867),
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              trailing:Icon(Icons.ice_skating,color:Colors.transparent),
-            ),
-          ),
-        ),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: SignUp(),
     );
   }
 }
+
