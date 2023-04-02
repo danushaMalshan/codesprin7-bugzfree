d8058d7f69c4aa13976aaa710020aed487c8e875
Danusha Malshan <dan.malshan@gmail.com>
2023-04-02 22:53:28 +0530

    git bug fixed

--- together/lib/main.dart
+++ together/lib/main.dart
@@ -1,43 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_framework/responsive_framework.dart';
-import 'package:together/screens/auth/signin.dart';
-import 'package:together/screens/select_category.dart';
+import 'user_details.dart';
+import 'profile.dart';
+import 'publish_event.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: const Color(0xFF142867),
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white,
-            centerTitle: true,
-            iconTheme: IconThemeData(
-              color: Color(0xFF142867),
-            ),
-            elevation: 2,
-          ),
-          scaffoldBackgroundColor: const Color(0xFFFFFFFF),
-          fontFamily: 'Poppins'),
-      builder: (context, child) => ResponsiveWrapper.builder(child,
-          maxWidth: 1200,
-          minWidth: 480,
-          defaultScale: true,
-          breakpoints: [
-            const ResponsiveBreakpoint.resize(480, name: MOBILE),
-            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-            const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-          ],
-          background: Container(color: const Color(0xFFF5F5F5))),
-      home: const SelectCategoryScreen(),
+      home: eventPublish(),
     );
   }
 }
