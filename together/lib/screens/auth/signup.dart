3c7e31be735c6fe0867cd852b0fdb559e7596df1
vinukavinnath <wlvvinnath@gmail.com>
2023-04-04 22:17:57 +0530

    SignUp page added for second time with errors

---
+++ together/lib/screens/auth/signup.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            const Image(
+              image: AssetImage("Assets/Images/Logo.jpg"),
+              height: 350,
+              width: 300,
+            ),
+            const SizedBox(height: 25),
+            loginButtons("Continue with Apple", "Assets/Images/apple_logo.png",
+                Colors.white, Colors.black),
+            loginButtons(
+              "Continue with Facebook",
+              "Assets/Images/facebook.png",
+              Colors.white,
+              const Color(0xff1877f2),
+            ),
+            loginButtons(
+              "Continue with Google",
+              "Assets/Images/google.png",
+              const Color(0xff142867),
+              const Color(0xffd9d9d9),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                tileColor: const Color(0xffd9d9d9),
+                leading: const Icon(Icons.mail_outline_rounded, size: 32),
+                title: const Center(
+                  child: Text(
+                    "Continue with Email",
+                    style: TextStyle(
+                      color: Color(0xff142867),
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                trailing: const Icon(Icons.ice_skating, color: Colors.transparent),
+              ),
+            ),
+            const Text("Already have an account?"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget loginButtons(
+    String title, String imagePath, Color titleColor, Color backColor) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 30),
+    child: ListTile(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      tileColor: backColor,
+      leading: Image.asset(
+        imagePath,
+        height: 32,
+      ),
+      title: Center(
+        child: Text(
+          title,
+          style: TextStyle(
+            color: titleColor,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      trailing: const Icon(Icons.ice_skating, color: Colors.transparent),
+    ),
+  );
+}
