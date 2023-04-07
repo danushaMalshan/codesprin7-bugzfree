4f3943367665505c684f90c6aee0142489d89915
vinukavinnath <wlvvinnath@gmail.com>
2023-04-07 12:09:19 +0530

    Issue assign to Danusha

--- together/lib/main.dart
+++ together/lib/main.dart
@@ -37,24 +37,21 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFFFFFFF),
         fontFamily: 'Poppins',
       ),
-      builder: (context, child) => ResponsiveWrapper.builder(child,
-          maxWidth: 1200,
-          minWidth: 480,
-          defaultScale: true,
-          breakpoints: [
-            const ResponsiveBreakpoint.resize(480, name: MOBILE),
-            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-            const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-          ],
-          background: Container(color: const Color(0xFFF5F5F5))),
-
-
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 1200,
+        minWidth: 480,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(480, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+        ],
+        background: Container(
+          color: const Color(0xFFF5F5F5),
+        ),
+      ),
       home: myAllMessages(),
-
-
-
-
-
     );
   }
 }
