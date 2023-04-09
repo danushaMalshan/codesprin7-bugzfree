b233316867e45399c6d8fedb146d3b75e62082b2
vinukavinnath <wlvvinnath@gmail.com>
2023-04-09 13:05:21 +0530

    Pending Reminders page completed

--- together/lib/screens/pending_reminders.dart
+++ together/lib/screens/pending_reminders.dart
@@ -20,8 +20,8 @@ class _PendingRemindersState extends State<PendingReminders> {
           child: ListView(
             children: <Widget>[
               Container(
-                margin: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 10.0),
+                margin: const EdgeInsets.only(
+                    left: 10.0, top: 20.0, right: 10.0, bottom: 0.0),
                 child: Text(
                   'Pending Reminders',
                   textAlign: TextAlign.center,
@@ -29,9 +29,10 @@ class _PendingRemindersState extends State<PendingReminders> {
                 ),
               ),
               Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                margin: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 15.0),
                 padding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 10.0),
+                    vertical: 15.0, horizontal: 10.0),
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20.0),
@@ -40,8 +41,14 @@ class _PendingRemindersState extends State<PendingReminders> {
                 child: Column(
                   children: <Widget>[
                     ListTile(
-                      title: Text('Bentota Beach Fiesta', style: forEventName(),),
-                      subtitle: Text('Will be reminded on Monday'),
+                      title: Text(
+                        'Bentota Beach Fiesta',
+                        style: forEventName(),
+                      ),
+                      subtitle: Text(
+                        'Will be reminded on Monday',
+                        style: forEventSub(),
+                      ),
                     ),
                     const Divider(
                       color: Color(0xff142867),
@@ -52,15 +59,12 @@ class _PendingRemindersState extends State<PendingReminders> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: <Widget>[
-
-                          Icon(
-                            Icons.add_circle,
-                            size: 60.0,
-                          ),
-                          Icon(
-                            Icons.arrow_circle_right_rounded,
-                            size: 60.0,
-                          ),
+                          pendingReminderIcons(const Icon(Icons.delete),
+                              'Remove from Pending Reminders'),
+                          pendingReminderIcons(
+                              const Icon(Icons.add_circle), 'Add to Reminders Now'),
+                          pendingReminderIcons(const Icon(Icons.arrow_circle_right),
+                              'Go to Event Details'),
                         ],
                       ),
                     ),
@@ -85,8 +89,26 @@ TextStyle messagePreviewTextStyle() {
 
 TextStyle forEventName() {
   return const TextStyle(
-    fontSize: 25.0,
+    fontSize: 27.0,
     // fontWeight: FontWeight.bold,
-    color: Colors.black,
+    color: Color(0xff142867),
+  );
+}
+
+TextStyle forEventSub() {
+  return const TextStyle(
+    fontSize: 17.0,
+    // fontWeight: FontWeight.bold,
+    color: Color(0xff142867),
+  );
+}
+
+Widget pendingReminderIcons(Icon remIcon, String remToolTip) {
+  return IconButton(
+    icon: remIcon,
+    onPressed: () {},
+    tooltip: remToolTip,
+    iconSize: 45.0,
+    // color: Color(0xff142867),
   );
 }
